332652731d6ccfcea55ccd11c0139b17e2a110df
Shengyou Fan <shengyoufan@gmail.com>
2022-09-12 16:01:45 +0800

    connect to database in API

--- src/main/kotlin/io/kraftsman/plugins/BookApi.kt
+++ src/main/kotlin/io/kraftsman/plugins/BookApi.kt
@@ -3,8 +3,16 @@ package io.kraftsman.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.Database
 
 fun Application.configureBookApi() {
+    Database.connect(
+        url = "jdbc:mysql://localhost:8889/sample",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "root"
+    )
+
     routing {
         get("/api/v1/books") {
             call.respond(mapOf("data" to "books"))
